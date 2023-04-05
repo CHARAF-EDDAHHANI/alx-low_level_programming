666f45d4a5f6863aa406f8c74d09c34ada4b9742
charaf <charaf.gdm@gmail.com>
2023-04-05 07:28:06 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *_puts_recursion - prints a string, followed by a new line
- *@s: string to print
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
@@ -12,5 +12,5 @@ void _puts_recursion(char *s)
 		return;
 	}
 	_putchar(*s);
-	_ptus_recursion(s+1);
+	_puts_recursion(s + 1);
 }
