50fc47a3278d26056c381c748d53530ad0a10b5c
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2023-06-21 14:13:26 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/HONORPRIDE/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom%20(1).so
-export LD_PRELOAD=/tmp/nrandom%20(1).so
+wget -P /tmp https://github.com/HONORPRIDE/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom(1).so
+export LD_PRELOAD=/tmp/nrandom(1).so
