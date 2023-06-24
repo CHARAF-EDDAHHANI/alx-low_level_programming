babf17ed7c5cf031ab3e0f51e6fc37ffea83969f
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2023-06-24 11:42:06 +0100

    Update 4-strpbrk.c

--- 0x18-dynamic_libraries/4-strpbrk.c
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -11,7 +11,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+        int i, j;
 
-	for (i = 0; *s != '\0'; i++)
-	{
+        for (i = 0; *s != '\0'; i++)
+        {
+                for (j = 0; accept[j] != '\0'; j++)
+                {
+                        if (*s == accept[j])
+                        {
+                                return (s);
+                        }
+                }
+                s++;
+        }
+
+        return (NULL);
+}
